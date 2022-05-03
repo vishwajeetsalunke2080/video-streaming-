3684793a9baeb7bc565da74ca1b8d39ead244e2a
Vishwajeet Salunke <salunkevishwajeet577@gmail.com>
2022-05-03 23:17:29 +0530

    Updated js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const {MongoClient} = require ('mongodb');
 const readline = require("readline");
 const database = "helloworld";
-const url = "mongodb+srv://vishwajeet:5824sj70c1ygYT9R@db-mongodb-blr1-30794-dc36d27e.mongo.ondigitalocean.com/videobase?authSource=admin&replicaSet=db-mongodb-blr1-30794&tls=true&tlsCAFile=ca-certificate.crt";
+const url = "";
 const client = new MongoClient(url);
 
 async function connect(){
